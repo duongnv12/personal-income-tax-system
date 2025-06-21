5c03dd28d45e4e27d10f79040f9a286108ea8a4d
duongnv12 <23012731@st.phenikaa-uni.edu.vn>
2025-06-21 13:28:57 +0000

    refactor: implement fillable and casts properties in TaxBracket model

--- app/Models/TaxBracket.php
+++ app/Models/TaxBracket.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TaxBracket extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'level',
+        'min_income',
+        'max_income',
+        'tax_rate',
+        'effective_date',
+    ];
+
+    protected $casts = [
+        'min_income' => 'integer',
+        'max_income' => 'integer',
+        'tax_rate' => 'float', // Cast tax_rate sang float
+        'effective_date' => 'date',
+    ];
 }
